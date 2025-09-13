eaca7f2b08b57cbe505753638145c5041eaae473
Alejandro Vinokur <ajvvja13@gmail.com>
2025-09-13 16:06:13 -0300

    POR-XX modified services cards

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# Crear directorio y archivo SQLite si es necesario y no existen
+if grep -q '^DB_CONNECTION=sqlite' /var/www/html/.env; then
+  if [ ! -d /var/www/html/database ]; then
+    mkdir -p /var/www/html/database
+    chown devuser:devgroup /var/www/html/database
+  fi
+  if [ ! -f /var/www/html/database/database.sqlite ]; then
+    touch /var/www/html/database/database.sqlite
+    chown devuser:devgroup /var/www/html/database/database.sqlite
+    chmod 664 /var/www/html/database/database.sqlite
+  fi
+fi
+
 # Setup a cron schedule
 echo "* * * * * cd /var/www/html && /usr/local/bin/php artisan schedule:run >> /var/log/cron.log 2>&1
 # This extra line makes it a valid cron" > /scheduler.txt
